1719c80acb6b8bc5e90a0221c94df1e8f408358d
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-10-05 12:22:04 +0200

    fixing routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import { Auth } from "./components/auth"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+// Components & Context
+import { AuthProvider } from './contexts/AuthContext';
+import {Auth} from './components/Auth';
+import Navigation from './components/Navigation';
+import RestaurantsList from './pages/RestaurantsList';
+import RestaurantDetails from './pages/RestaurantDetails';
 
+const App = () => {
   return (
-    <>
-      <div className='App'>
-        <Auth/>
-      </div>
-    </>
-  )
+    <AuthProvider>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<div>Hem</div>} />
+          <Route path="/search" element={<div>Sök</div>} />
+          <Route path="/location" element={<div>Min position</div>} />
+          <Route path="/admin" element={<div>Admin-panel</div>} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/RestaurantsList" element={<RestaurantsList/>} />
+          <Route path="/restaurant/:id" element={<RestaurantDetails />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
