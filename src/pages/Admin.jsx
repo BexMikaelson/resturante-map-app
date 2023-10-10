ed3e8f1f62e7a33a950ec481e45436368c16f2ff
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-10-10 15:40:14 +0200

    Admin page for admin component

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,11 @@
+import AdminDashboard from "../components/AdminDashboard";
+
+const adminPanel = () => {
+    return ( 
+        <div>
+            <AdminDashboard/>
+        </div>
+     );
+}
+ 
+export default adminPanel;
