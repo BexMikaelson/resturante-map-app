eff43121c3b510afda0a98d6382ddf8c5cc85edf
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-10-10 15:46:18 +0200

    restaurants page

---
+++ src/pages/Restaurants.jsx
@@ -0,0 +1,11 @@
+import RestaurantsList from "../components/RestaurantsList";
+
+const RestaurantsPage = () => {
+    return ( 
+        <div>
+            <RestaurantsList/>
+        </div>
+     );
+}
+ 
+export default RestaurantsPage;
