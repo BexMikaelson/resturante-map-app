4bc8904fa305ccb88621070e3d978b0f899798ad
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-11-13 22:44:49 +0100

    changed navigation to use NavLink

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,30 +1,50 @@
 import React from 'react';
-import { useAuth} from '../contexts/AuthContext';
+import { useAuth } from '../contexts/AuthContext';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
-   const { user} = useAuth();
+  const { user } = useAuth();
 
   return (
     <Navbar expand="md" bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand><Nav.Link href="/">The Pizza Map</Nav.Link></Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/">The Pizza Map</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/RestaurantsPage">Restauranger</Nav.Link>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/" exact>
+                Hem
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/RestaurantsPage">
+                Restauranger
+              </Nav.Link>
+            </Nav.Item>
             {user ? (
-            <>
-            <Nav.Link href="/auth">Logga in/Registrera</Nav.Link>
-            <Nav.Link href="/adminPanel">Admin-panel</Nav.Link>
-            </>
-            ):(
-            <Nav.Link href="/auth">Logga in/Registrera</Nav.Link>
+              <>
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="/auth">
+                    Logga in/Registrera
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="/adminPanel">
+                    Admin-panel
+                  </Nav.Link>
+                </Nav.Item>
+              </>
+            ) : (
+              <Nav.Item>
+                <Nav.Link as={NavLink} to="/auth">
+                  Logga in/Registrera
+                </Nav.Link>
+              </Nav.Item>
             )}
- 
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -33,3 +53,4 @@ const Navigation = () => {
 }
 
 export default Navigation;
+
